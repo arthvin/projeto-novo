25c665dde866dd422b5f0d7999cf33c742e4f285
arthurSemensati <arth.vin1408@gmail.com>
2024-09-22 17:50:17 -0300

    Commit

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,8 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    { nome: 'Liderança em Design', id: 1, src: livro },
+    { nome: 'Javascript', id: 2, src: livro },
+    { nome: 'Poetas do Amanhã', id: 3, src: livro },
+    { nome: 'Pé de muleque', id: 4, src: livro },
+]
